5759a3f89e3fa3920811f4036521f728ce3de762
chbird <birdchristian96@gmail.com>
2024-11-18 18:30:22 -0800

    test updates

--- project-3-Maven/src/test/java/JsonParserVisitorTest.java
+++ project-3-Maven/src/test/java/JsonParserVisitorTest.java
@@ -1,5 +1,4 @@
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 
 import java.io.File;
 import java.net.URL;
@@ -18,7 +17,6 @@ public class JsonParserVisitorTest {
  */
 @Test
 public void testVisitorWithNewsApiFileSource() throws Exception {
-    // Arrange
     String testFilePath = "test_newsapi_file.json";
     ParserConfig config = new ParserConfig(ParserSource.FILE, ParserFormat.NewsAPI, testFilePath);
     File testFile = new File(testFilePath);
@@ -26,13 +24,10 @@ public void testVisitorWithNewsApiFileSource() throws Exception {
     NewsApiJsonParser newsApiParser = mock(NewsApiJsonParser.class);
     JsonParserVisitor visitor = new JsonParserVisitor(config);
     
-    // Act
     visitor.visit(newsApiParser);
     
-    // Assert: Check that parse is called for NewsAPI with File source
     verify(newsApiParser, times(1)).parse(testFile); // Verify interaction with mock
     
-    // Additional assert to check that the visitor is indeed using a file source
     assertEquals(ParserSource.FILE, config.getSource(), "Source should be FILE for NewsAPI.");
     assertEquals(ParserFormat.NewsAPI, config.getFormat(), "Format should be NewsAPI for this test.");
 }
@@ -46,7 +41,6 @@ public void testVisitorWithNewsApiFileSource() throws Exception {
  */
 @Test
 public void testVisitorWithNewsApiUrlSource() throws Exception {
-    // Arrange
     String testUrlString = "http://example.com/test_newsapi_url.json";
     ParserConfig config = new ParserConfig(ParserSource.URL, ParserFormat.NewsAPI, testUrlString);
     URL testUrl = new URL(testUrlString);
@@ -54,13 +48,10 @@ public void testVisitorWithNewsApiUrlSource() throws Exception {
     NewsApiJsonParser newsApiParser = mock(NewsApiJsonParser.class);
     JsonParserVisitor visitor = new JsonParserVisitor(config);
     
-    // Act
     visitor.visit(newsApiParser);
     
-    // Assert: Check that parse is called for NewsAPI with URL source
     verify(newsApiParser, times(1)).parse(testUrl); // Verify interaction with mock
     
-    // Additional assert to check that the visitor is indeed using a URL source
     assertEquals(ParserSource.URL, config.getSource(), "Source should be URL for NewsAPI.");
     assertEquals(ParserFormat.NewsAPI, config.getFormat(), "Format should be NewsAPI for this test.");
 }
@@ -74,7 +65,6 @@ public void testVisitorWithNewsApiUrlSource() throws Exception {
  */
 @Test
 public void testVisitorWithSimpleJsonFileSource() throws Exception {
-    // Arrange
     String testFilePath = "test_simple_file.json";
     ParserConfig config = new ParserConfig(ParserSource.FILE, ParserFormat.SIMPLE, testFilePath);
     File testFile = new File(testFilePath);
@@ -82,13 +72,10 @@ public void testVisitorWithSimpleJsonFileSource() throws Exception {
     SimpleJsonParser simpleJsonParser = mock(SimpleJsonParser.class);
     JsonParserVisitor visitor = new JsonParserVisitor(config);
     
-    // Act
     visitor.visit(simpleJsonParser);
     
-    // Assert: Check that parse is called for SimpleJson with File source
     verify(simpleJsonParser, times(1)).parse(testFile); // Verify interaction with mock
     
-    // Additional assert to check that the visitor is indeed using a file source
     assertEquals(ParserSource.FILE, config.getSource(), "Source should be FILE for SIMPLE format.");
     assertEquals(ParserFormat.SIMPLE, config.getFormat(), "Format should be SIMPLE for this test.");
 }
@@ -100,7 +87,6 @@ public void testVisitorWithSimpleJsonFileSource() throws Exception {
  */
 @Test
 public void testVisitorWithSimpleJsonUrlSourceNotSupported() {
-    // Arrange
     String testUrlString = "http://example.com/test_simple_url.json";
     ParserConfig config = new ParserConfig(ParserSource.URL, ParserFormat.SIMPLE, testUrlString);
     URL testUrl = null;
@@ -111,7 +97,6 @@ public void testVisitorWithSimpleJsonUrlSourceNotSupported() {
     SimpleJsonParser simpleJsonParser = mock(SimpleJsonParser.class);
     JsonParserVisitor visitor = new JsonParserVisitor(config);
     
-    // Act & Assert: Ensure Simple format does not support URL source
     UnsupportedOperationException exception = assertThrows(UnsupportedOperationException.class, () -> visitor.visit(simpleJsonParser));
     assertEquals("SIMPLE format does not support URL source.", exception.getMessage(), "Exception message should indicate unsupported URL source for SIMPLE format.");
 }
@@ -123,18 +108,14 @@ public void testVisitorWithSimpleJsonUrlSourceNotSupported() {
  */
 @Test
 public void testVisitorParserTypeConsistency() {
-    // Arrange for NewsAPI format with FILE source
     ParserConfig newsApiFileConfig = new ParserConfig(ParserSource.FILE, ParserFormat.NewsAPI, "project_3/inputs/newsapi.txt");
     JsonParserVisitor newsApiVisitor = new JsonParserVisitor(newsApiFileConfig);
     
-    // Assert that the NewsAPI parser type is used
     assertTrue(newsApiVisitor instanceof JsonParserVisitor, "Visitor type should be JsonParserVisitor for NewsAPI.");
     
-    // Arrange for SIMPLE format with FILE source
     ParserConfig simpleFileConfig = new ParserConfig(ParserSource.FILE, ParserFormat.SIMPLE, "project_3/inputs/simple.txt");
     JsonParserVisitor simpleVisitor = new JsonParserVisitor(simpleFileConfig);
     
-    // Assert that the SimpleJsonParser type is used
     assertTrue(simpleVisitor instanceof JsonParserVisitor, "Visitor type should be JsonParserVisitor for SIMPLE.");
 }
 }
